187117ca49bb6b1122584e06c58cf765163eac9d
iam1shel <luzmishelrt13@gmail.com>
2026-08-27 23:19:48 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana_02/Laboratorio_02/lab02CarritoKotlin/app/src/main/java/com/rojastuesta/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.rojastuesta.lab02carritokotlin
+
+fun main(){
+    println("=========================================")
+    println("   CARRITO DE COMPRAS - TIENDA TECSUP   ")
+    println("=========================================")
+}
