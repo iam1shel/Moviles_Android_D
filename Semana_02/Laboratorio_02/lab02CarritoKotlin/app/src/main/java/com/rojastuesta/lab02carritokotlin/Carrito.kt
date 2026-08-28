ce2ad6b10baee4ec17a21acb0309e492a95967f6
iam1shel <luzmishelrt13@gmail.com>
2026-08-27 23:55:13 -0500

    Agrega data class Producto y funciones de subtotal, IGV y total

--- Semana_02/Laboratorio_02/lab02CarritoKotlin/app/src/main/java/com/rojastuesta/lab02carritokotlin/Carrito.kt
+++ Semana_02/Laboratorio_02/lab02CarritoKotlin/app/src/main/java/com/rojastuesta/lab02carritokotlin/Carrito.kt
@@ -1,7 +1,48 @@
 package com.rojastuesta.lab02carritokotlin
 
+data class Producto(
+    val nombre:String,
+    val precio: Double,
+    var cantidad: Int
+)
+
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+fun calcularIGV(subtotal: Double): Double {
+    return subtotal * 0.18
+}
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    return subtotal + igv
+}
+
 fun main(){
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP   ")
     println("=========================================")
+    val nombreCliente = "Mishel Rojas"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
+
+    carrito.add(Producto("Laptop HP", 2500.0,1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Tablet Honor", 500.5, 3))
+    carrito.add(Producto("Impresora Epson", 300.0, 5))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+    println("Subtotal : S/ ${"%.2f".format(subtotal)}")
+    println("IGV (18%): S/ ${"%.2f".format(igv)}")
+    println("TOTAL    : S/ ${"%.2f".format(total)}")
 }
