0ac906fc2f6974e0aba355693c61537633269f01
iam1shel <luzmishelrt13@gmail.com>
2026-09-04 11:35:20 -0500

    Agrego bienvenida en Kotlin en la portada del repo.

---
+++ Bienvenida.kt
@@ -0,0 +1,5 @@
+fun main() {
+    println("Hola, soy Luz Mishel Rojas Tuesta.")
+    println("Este repo tiene los laboratorios del curso de Programacion Moviles.")
+    println("El codigo de cada lab esta en las ramas sin-ia y con-ia.")
+}
